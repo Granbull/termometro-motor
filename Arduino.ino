70fe41905d5ef1e8d459575408921fd7d65e0d2b
Granbull <37772503+Granbull@users.noreply.github.com>
2024-11-02 16:16:10 -0300

    Função LCD

--- Arduino.ino
+++ Arduino.ino
@@ -1,10 +1,22 @@
 #include <Thermistor.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 Thermistor temp(0);
 int maiorTemp;
 int menorTemp = 9999;
 void setup() {
   Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print("Estacio 2024-2");
+  lcd.setCursor(2, 1);
+  lcd.print("Iniciando...");
+  delay(2500);
+  lcd.clear();
 }
 void loop() {
   int temperatura = temp.getTemp();
@@ -16,12 +28,29 @@ void loop() {
   }
   Serial.print("Temperatura atual: ");
   Serial.print(temperatura);
-  Serial.println("°C");
+  Serial.println("°c");
   Serial.print("Mínima: ");
   Serial.print(menorTemp);
   Serial.print("°C | Máxima: ");
   Serial.print(maiorTemp);
   Serial.println("°C");
-  Serial.println("----------------------------"); 
+  Serial.println("----------------------------");
+
+  lcd.setCursor(0, 0);
+  lcd.print("Min:| Temp |Max:");
+
+  lcd.setCursor(0, 1);
+  lcd.print(menorTemp);
+  lcd.print("\xDF""c|");
+
+  lcd.setCursor(6, 1);
+  lcd.print(temperatura);
+  lcd.print("\xDF""c");
+
+  lcd.setCursor(11, 1);
+  lcd.print("|");
+  lcd.print(maiorTemp);
+  lcd.print("\xDF""c");
+
   delay(10000);
 }
