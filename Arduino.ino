a263a1247f81204355f1c9fc2bd95c98d7d9743d
Granbull <37772503+Granbull@users.noreply.github.com>
2024-11-02 15:14:00 -0300

    Função de temperatura mínima/máxima

--- Arduino.ino
+++ Arduino.ino
@@ -1,13 +1,27 @@
 #include <Thermistor.h>
 
 Thermistor temp(0);
+int maiorTemp;
+int menorTemp = 9999;
 void setup() {
   Serial.begin(9600);
 }
 void loop() {
   int temperatura = temp.getTemp();
-  Serial.print("Temperatura: ");
+  if (temperatura > maiorTemp) {
+    maiorTemp = temperatura;
+  }
+  if (temperatura < menorTemp) {
+    menorTemp = temperatura;
+  }
+  Serial.print("Temperatura atual: ");
   Serial.print(temperatura);
   Serial.println("°C");
-  delay(1000);
+  Serial.print("Mínima: ");
+  Serial.print(menorTemp);
+  Serial.print("°C | Máxima: ");
+  Serial.print(maiorTemp);
+  Serial.println("°C");
+  Serial.println("----------------------------"); 
+  delay(10000);
 }
